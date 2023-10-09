7f40ffb90f58f84c5ac29d92d69d41845be4261f
parivesh.thulung <pariveshthulung@gmail.com>
2023-10-09 20:58:11 +0545

    add public layout

---
+++ ebay/Controllers/PublicController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ebay.Controllers;
+
+[AllowAnonymous]
+public class PublicController :Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
