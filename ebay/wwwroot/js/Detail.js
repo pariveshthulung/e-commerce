cd5ed2e73f3fb22ed32e5496722e176b51ba2042
parivesh.thulung <pariveshthulung@gmail.com>
2023-12-05 15:08:41 +0545

    add detail css

--- ebay/wwwroot/js/Detail.js
+++ ebay/wwwroot/js/Detail.js
@@ -10,3 +10,16 @@ function myFunction(imgs) {
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
+
+  function myFunction2(imgs) {
+    // Get the expanded image
+    var expandImg2 = document.getElementById("expandedImg2");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg2.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg2.parentElement.style.display = "block";
+  }
